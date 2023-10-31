b8538b915913eadba63db756d6312b5ca354509b
RLau <cfgfung@gmail.com>
2023-10-30 22:20:40 -0700

    Small fix: expose the time class.

--- torch_frame/__init__.py
+++ torch_frame/__init__.py
@@ -1,5 +1,5 @@
 from .stype import (stype, numerical, categorical, text_embedded,
-                    multicategorical)
+                    multicategorical, time)
 from .data import TensorFrame
 from .typing import TaskType, DataFrame, NAStrategy
 from torch_frame.utils import save, load, cat  # noqa
